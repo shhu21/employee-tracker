81294aaa04accc33e6decc558d10c4a440e93fbc
shhu21 <hu.sara21@gmail.com>
2020-08-08 13:17:13 -0700

    fixes broken queries and duplicate choice arrays

--- utils/queries.js
+++ utils/queries.js
@@ -6,7 +6,8 @@ const getDepts = () => {
             if (err) throw err;
             console.log('\n');
             console.table(result);
-    });
+        }
+    );
 }
 
 const getRoles = () => {
@@ -19,12 +20,13 @@ const getRoles = () => {
             if (err) throw err;
             console.log('\n');
             console.table(result);
-    });
+        }
+    );
 }
 
 const getEmployees = () => {
     connection.promise().query(
-        `SELECT e.id, e.first_name, e.last_name, title, salary, name AS 'Department', CONCAT(m.first_name, m.last_name) AS 'Manager'
+        `SELECT e.id, e.first_name, e.last_name, title, salary, name AS 'Department', CONCAT(m.first_name, ' ', m.last_name) AS 'Manager'
             FROM employee e
             LEFT JOIN role r
             ON e.role_id = r.id
@@ -36,7 +38,8 @@ const getEmployees = () => {
             if (err) throw err;
             console.log('\n');
             console.table(result);
-    });
+        }
+    );
 }
 
 const insertDept = (dept) => {
@@ -46,57 +49,65 @@ const insertDept = (dept) => {
             if (err) throw err;
     
             console.log('\nDepartment successfully added.');
-    });
+        }
+    );
 }
 
 const insertRole = (role) => {
-    connection.promise().query(`INSERT INTO role (title, salary, department_id)
+    connection.promise().query(
+        `INSERT INTO role (title, salary, department_id)
         VALUES (
             '${role.role}', 
             ${role.salary}, 
             (SELECT id FROM department
-            WHERE name = '${role.dept}';
+            WHERE name = '${role.dept}');
         )`,
         (err, result) => {
             if (err) throw err;
             
             console.log('\nRole successfully added.');
-    });
+        }
+    );
 
 }
 
 const insertEmployee = (employee) => {
     const manager = employee.manager.split(' ');
 
-    connection.promise().query(`INSERT INTO employee (first_name, last_name, role_id, manager_id)
-        VALUES (
-            '${employee.firstName}', 
-            '${employee.lastName}', 
-            (SELECT id FROM role
-                WHERE role.title = '${employee.role}'), 
-            (SELECT e.id FROM employee e
-                    WHERE e.first_name = '${manager[0]}'
-                    AND e.last_name = '${managaer[1]}')
-        );`,
+    connection.promise().query(`
+        INSERT INTO employee (first_name, last_name, role_id, manager_id)
+            VALUES (
+                '${employee.firstName}', 
+                '${employee.lastName}', 
+                (SELECT id FROM role
+                    WHERE role.title = '${employee.role}'), 
+                (SELECT e.id FROM employee e
+                        WHERE e.first_name = '${manager[0]}'
+                        AND e.last_name = '${managaer[1]}')
+            );`,
         (err, result) => {
             if (err) throw err;
                     
-                console.log('\nEmployee successfully added.');
-        });
+            console.log('\nEmployee successfully added.');
+        }
+    );
 
 }
 
 const updateEmployee = (employee) => {
-    const name = employee.split(' ');
-    connection.promise().query(`UPDATE employee
-        SET role_id = (SELECT id FROM role WHERE title = ${employee.role})
-        WHERE employee.first_name = '${name[0]}'
-        AND employee.last_name = '${name[1]}';`,
+    const name = employee.employee.split(' ');
+
+    connection.promise().query(`
+        UPDATE employee
+            SET role_id = (SELECT id FROM role WHERE title = '${employee.role}')
+            WHERE first_name = '${name[0]}'
+            AND last_name = '${name[1]}';`,
         (err, result) => {
             if (err) throw err;
     
-                console.log('\nEmployee successfully updated.');
-            });
+            console.log('\nEmployee successfully updated.');
+        }
+    );
 }
 
 module.exports = { getDepts, getRoles, getEmployees, insertDept, insertRole, insertEmployee, updateEmployee };
--- utils/start.js
+++ utils/start.js
@@ -29,132 +29,141 @@ const start = () => {
             }
             else if(choice.option == 'Add a role') {
                 connection.promise().query(`SELECT name FROM department;`, 
-                (err, result) => {
-                    if (err) throw err;
+                    (err, result) => {
+                        if (err) throw err;
 
-                    inquirer
-                        .prompt([
-                            {
-                                type: 'input',
-                                name: 'role',
-                                message: 'Enter the name of the role.',
-                                validate: input => {
-                                    if (input) {
-                                        return true;
-                                    } else {
-                                        console.log('\nPlease enter the name of the role.');
-                                        return false;
+                        inquirer
+                            .prompt([
+                                {
+                                    type: 'input',
+                                    name: 'role',
+                                    message: 'Enter the name of the role.',
+                                    validate: input => {
+                                        if (input) {
+                                            return true;
+                                        } else {
+                                            console.log('\nPlease enter the name of the role.');
+                                            return false;
+                                        }
                                     }
-                                }
-                            },
-                            {
-                                type: 'input',
-                                name: 'salary',
-                                message: 'Enter the salary of the role.',
-                                validate: input => {
-                                    if (input) {
-                                        return true;
-                                    } else {
-                                        console.log('\nPlease enter the salary of the role.');
-                                        return false;
+                                },
+                                {
+                                    type: 'input',
+                                    name: 'salary',
+                                    message: 'Enter the salary of the role.',
+                                    validate: input => {
+                                        if (input) {
+                                            return true;
+                                        } else {
+                                            console.log('\nPlease enter the salary of the role.');
+                                            return false;
+                                        }
                                     }
+                                },
+                                {
+                                    type: 'list',
+                                    name: 'dept',
+                                    message: 'Enter the department of the role.',
+                                    choices: result.map(res => res.name)
                                 }
-                            },
-                            {
-                                type: 'list',
-                                name: 'dept',
-                                message: 'Enter the department of the role.',
-                                choices: result.map(res => res.name)
+                            ])
+                            .then(role => {
+                                insertRole(role);
+                                start();
                             }
-                        ])
-                        .then(role => {
-                            insertRole(role);
-                            start();
-                        });
-                });
+                        );
+                    }
+                );
             }
-            else if(choice.option == 'Add an employee.') {
-                connection.promise().query(`SELECT title, CONCAT(first_name, ' ', last_name) AS 'name' 
-                FROM role r
-                INNER JOIN employee e
-                ON r.id = e.role_id;`, 
-                (err, result) => {
-                    if (err) throw err;
+            else if(choice.option == 'Add an employee') {
+                connection.promise().query(
+                    `SELECT DISTINCT title, CONCAT(first_name, ' ', last_name) AS 'name' 
+                        FROM role r
+                        INNER JOIN employee e
+                        ON r.id = e.role_id;`, 
+                    (err, result) => {
+                        if (err) throw err;
 
-                    inquirer
-                        .prompt([
-                            {
-                                type: 'input',
-                                name: 'firstName',
-                                message: 'Enter the employee\'s first name',
-                                validate: input => {
-                                    if (input) {
-                                        return true;
-                                    } else {
-                                        console.log('\nPlease enter the first name of the employee.');
-                                        return false;
+                        inquirer
+                            .prompt([
+                                {
+                                    type: 'input',
+                                    name: 'firstName',
+                                    message: 'Enter the employee\'s first name',
+                                    validate: input => {
+                                        if (input) {
+                                            return true;
+                                        } else {
+                                            console.log('\nPlease enter the first name of the employee.');
+                                            return false;
+                                        }
                                     }
-                                }
-                            },
-                            {
-                                type: 'input',
-                                name: 'lastName',
-                                message: 'Enter the employee\'s last name',
-                                validate: input => {
-                                    if (input) {
-                                        return true;
-                                    } else {
-                                        console.log('\nPlease enter the last name of the employee.');
-                                        return false;
+                                },
+                                {
+                                    type: 'input',
+                                    name: 'lastName',
+                                    message: 'Enter the employee\'s last name',
+                                    validate: input => {
+                                        if (input) {
+                                            return true;
+                                        } else {
+                                            console.log('\nPlease enter the last name of the employee.');
+                                            return false;
+                                        }
                                     }
+                                },
+                                {
+                                    type: 'list',
+                                    name: 'role',
+                                    message: 'Choose the employee\'s role',
+                                    choices: [...new Set(result.map(res => res.title))]
+                                },
+                                {
+                                    type: 'list',
+                                    name: 'manager',
+                                    message: 'Choose the employee\'s manager',
+                                    choices: result.map(res => res.name)
                                 }
-                            },
-                            {
-                                type: 'list',
-                                name: 'role',
-                                message: 'Choose the employee\'s role',
-                                choices: result.map(res => res.title)
-                            },
-                            {
-                                type: 'list',
-                                name: 'manager',
-                                message: 'Choose the employee\'s manager',
-                                choices: result.map(res => res.name)
+                            ])
+                            .then(employee => {
+                                insertEmployee(employee);
+                                start();
                             }
-                        ])
-                        .then(employee => {
-                            insertEmployee(employee);
-                            start();
-                        });
-                });
+                        );
+                    }
+                );
             }
             else if(choice.option == 'Update an employee role') {
-                connection.promise().query(`SELECT title, CONCAT(first_name, ' ', last_name) AS 'name' 
-                FROM role r
-                INNER JOIN employee e
-                ON r.id = e.role_id;`,
-                (err, result) => {
-                    if (err) throw err;
-                    inquirer
-                        .prompt([
-                            {
-                                type: 'list',
-                                name: 'employee',
-                                message: 'Choose the employee to update',
-                                choices: result.map(res => res.name)
-                            },
-                            {
-                                type: 'list',
-                                name: 'role',
-                                message: 'Choose the role of the employee',
-                                choices: result.map(res => res.title)
+                connection.promise().query(
+                    `SELECT title, CONCAT(first_name, ' ', last_name) AS 'name' 
+                        FROM role r
+                        INNER JOIN employee e
+                        ON r.id = e.role_id;`,
+                    (err, result) => {
+                        if (err) throw err;
+                        
+                        inquirer
+                            .prompt([
+                                {
+                                    type: 'list',
+                                    name: 'employee',
+                                    message: 'Choose the employee to update',
+                                    choices: result.map(res => res.name)
+                                },
+                                {
+                                    type: 'list',
+                                    name: 'role',
+                                    message: 'Choose the role of the employee',
+                                    choices: [...new Set(result.map(res => res.title))]
+                                }
+                            ])
+                            .then(employee => {
+                                updateEmployee(employee);
+                                start();
                             }
-                        ])
-                        .then(employee => {
-                            updateEmployee(employee);
-                            start();
-                        });
-                });
+                        );
+                    }
+                );
             }
             else {
                 connection.end();
@@ -164,3 +173,26 @@ const start = () => {
 }
 
 module.exports = start;
+
+// TODO : fix UI
+
+// TODO: add bonus queries
+    // Update employee managers.
+        // use the view employees by manager query to set the inquirer prompt
+        // create an update function in queries.js
+
+    // View employees by manager.
+        // probably select distinct name from employee where id = manager_id
+
+    // View employees by department.
+        // select name, firstname, lastname left join department on employes where role_id = r.id, r.department_id = d.id
+
+    // Delete departments, roles, and employees.
+        // select department names, role names, employee names,
+            // separate prompts and quereies
+
+    // View the total utilized budget of a department—i.e., the combined salaries of all employees in that department.
+        // select name from department for the prompt list
+        // make a function in queries.js
+            // select salary from role left join department on role_id = d.id
+            // sum the salaries and console.log
