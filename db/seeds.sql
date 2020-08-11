4b3b13b79a2e867e2613fbbe4b66eced6e17a272
shhu21 <hu.sara21@gmail.com>
2020-08-11 02:08:22 -0700

    cleans up seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,23 +1,29 @@
 -- Template database data
 
 -- Template department data
-INSERT INTO department(name) VALUES ('Accounting');
-INSERT INTO department(name) VALUES ('Engineering');
-INSERT INTO department(name) VALUES ('Human Resources');
-INSERT INTO department(name) VALUES ('Sales');
+INSERT INTO department(name) 
+VALUES 
+    ('Accounting'),
+    ('Engineering'),
+    ('Human Resources'),
+    ('Sales');
 
 -- Template role data
-INSERT INTO role(title, salary, department_id) VALUES ('Engineer', 200000, 2);
-INSERT INTO role(title, salary, department_id) VALUES ('Accountant', 160000, 1);
-INSERT INTO role(title, salary, department_id) VALUES ('HR', 150000, 3);
-INSERT INTO role(title, salary, department_id) VALUES ('Sales', 170000, 4);
+INSERT INTO role(title, salary, department_id) 
+VALUES 
+    ('Engineer', 200000, 2),
+    ('Accountant', 160000, 1),
+    ('HR', 150000, 3),
+    ('Sales', 170000, 4);
 
--- Template Employee data
-INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ('John', 'Smith', 1, NULL);
-INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ('Jane', 'Doe', 1, 1);
-INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ('Eric', 'Hayes', 2, NULL);
-INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ('Liz', 'Mall', 2, 3);
-INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ('Max', 'Well', 3, NULL);
-INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ('Lucy', 'Lu', 3, 5);
-INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ('Luis', 'Dan', 4, NULL);
-INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ('Kim', 'Kale', 4, 7);
+-- Template employee data
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES 
+    ('John', 'Smith', 1, NULL),
+    ('Jane', 'Doe', 1, 1),
+    ('Eric', 'Hayes', 2, NULL),
+    ('Liz', 'Mall', 2, 3),
+    ('Max', 'Well', 3, NULL),
+    ('Lucy', 'Lu', 3, 5),
+    ('Luis', 'Dan', 4, NULL),
+    ('Kim', 'Kale', 4, 7);
